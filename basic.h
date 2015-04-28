6fe8fb7a23bbe7b661b28906646f12010424dd6a
Ramprakash K <ramprakash-k@users.noreply.github.com>
2015-04-28 16:02:05 +0530

    Update basic.h

--- basic.h
+++ basic.h
@@ -9,4 +9,24 @@ enum basicType
 	cstring
 };
 
+static string btToString(basicType t) {
+	switch(t) {
+		case cint: return "int";
+		case cfloat: return "float";
+		case cvoid: return "void";
+		case cstring: return "string";
+		default: return "";
+	}
+}
+
+static int sizeofbt(basicType t) {
+	switch(t) {
+		case cint: return sizeof(int);
+		case cfloat: return sizeof(float);
+		case cvoid: return 0;
+		case cstring: return 0;
+		default: return 0;
+	}
+}
+
 #endif // BASIC_H
