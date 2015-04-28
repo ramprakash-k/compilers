a9bc67cb6cf4583cdffd3a9299051574bb44f360
Ramprakash K <ramprakash-k@users.noreply.github.com>
2015-04-28 16:01:48 +0530

    Update classes.h

--- classes.h
+++ classes.h
@@ -20,26 +20,6 @@ extern int label;
 
 extern RegMan regman;
 
-static string btToString(basicType t) {
-	switch(t) {
-		case cint: return "int";
-		case cfloat: return "float";
-		case cvoid: return "void";
-		case cstring: return "string";
-		default: return "";
-	}
-}
-
-static int sizeofbt(basicType t) {
-	switch(t) {
-		case cint: return sizeof(int);
-		case cfloat: return sizeof(float);
-		case cvoid: return 0;
-		case cstring: return 0;
-		default: return 0;
-	}
-}
-
 struct typeExp {
 	typeExp *t;
 	basicType type;
